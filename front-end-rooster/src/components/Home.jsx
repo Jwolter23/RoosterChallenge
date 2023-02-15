3119c2773010a8d6a5fdf1b234c5f12407635f20
Jordan Wolter <jwolter456@gmail.com>
2023-02-15 17:36:16 -0500

    finished second section

--- front-end-rooster/src/components/Home.jsx
+++ front-end-rooster/src/components/Home.jsx
@@ -19,15 +19,24 @@ export default function Home () {
             <div className="sectionOne">
                 <div className="leftSection">
                     <div className="leftContent">
-                        <h1 className="leftSectionHeader">Lorem Ipsum</h1>
+                        <h1 className="leftSectionHeader">LOREM IPSUM</h1>
                         <p className="leftSectionBody">Lorem ipsum dolor sit amet, et felis intger. Cras ac, duis nisl magna est sociis, neque in odio vel, sit lobortis erat. Fugit quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet sem tellus suspendisse. Condimentum non aliquet sed, quique risus vitae semper duis feugiat.</p>
                         <button className="leftSectionButton">Lorem Ipsum</button>
                     </div>
                 </div>
                 <div className="rightSection">
-                    
                 </div>
+            </div>
+            <div className="sectionTwo">
+                <div className="leftSectionTwo">
+                    <div className="leftContentTwo">
+                    <h1 className="leftSectionHeaderTwo">LOREM IPSUM</h1>
+                    <button className="leftSectionButtonTwo">BUTTON BUTTON</button>
+                    </div>
+                </div>
+                <div className="rightSectionTwo">
 
+                </div>
             </div>
         </div>
     )
