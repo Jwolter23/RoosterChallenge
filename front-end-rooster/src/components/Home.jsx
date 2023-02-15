b5af2f4b0d7a334cb83bfaaad978d412d0e00c18
Jordan Wolter <jwolter456@gmail.com>
2023-02-15 16:01:52 -0500

    centered everything on screen and created button

--- front-end-rooster/src/components/Home.jsx
+++ front-end-rooster/src/components/Home.jsx
@@ -1,10 +1,19 @@
 export default function Home () {
     return(
         <div className="wholeScreen">
-            
             <div className="welcomeScreen">
-                <div className="roosterLogoBox">
-                <h1 className="roosterLogo">RoosterGrin</h1>
+                    <div className="roosterLogoBox">
+                        <h1 className="roosterLogo">RoosterGrin</h1>
+                    </div>
+                <div className="welcomeContent">
+                    
+                    <div className="welcomeText">
+                        <h1 className="welcomeTo">WELCOME TO</h1>
+                        <h2 className="roosterGrin">Rooster Grin</h2>
+                    </div>
+                    <div className="welcomeButtonBox">
+                        <button className="welcomeButton">Explore</button>
+                    </div>
                 </div>
             </div>
         </div>
