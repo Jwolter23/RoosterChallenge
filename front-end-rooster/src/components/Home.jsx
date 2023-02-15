b3b126516ef895a4d43f17224c7fc7f9a6bf3837
Jordan Wolter <jwolter456@gmail.com>
2023-02-15 17:38:00 -0500

    small fix

--- front-end-rooster/src/components/Home.jsx
+++ front-end-rooster/src/components/Home.jsx
@@ -35,7 +35,6 @@ export default function Home () {
                     </div>
                 </div>
                 <div className="rightSectionTwo">
-
                 </div>
             </div>
         </div>
